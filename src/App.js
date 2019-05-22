90b47b652eaf63dcb58c3ad1f67b8bd7ab046587
Brian Bartholomew <bartholomewbd@gmail.com>
2019-05-22 09:19:07 -0400

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 import MeteorList from "./components/MeteorList";
+import SearchBox from "./components/SearchBox";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      searchValue: "",
       data: []
     };
   }
+
+  onSearchChange = e => {
+    this.setState({ searchValue: e.target.value });
+    console.log(this.state.searchValue);
+  };
 
   componentDidMount() {
     fetch("https://data.nasa.gov/resource/gh4g-9sfh.json")
@@ -20,8 +27,20 @@ class App extends Component {
 
   render() {
     const { data } = this.state;
+    const findMatches = (wordToMatch, meteorites) => {
+      return meteorites.filter(meteorite => {
+        const regex = new RegExp(wordToMatch, "gi");
+        return meteorite.name.match(regex);
+      });
+    };
+    const filteredMeteorites = findMatches(this.state.searchValue, data);
 
-    return <MeteorList meteorites={data} />;
+    return (
+      <div>
+        <SearchBox searchChange={this.onSearchChange} />
+        <MeteorList meteorites={filteredMeteorites} />
+      </div>
+    );
   }
 }
 
