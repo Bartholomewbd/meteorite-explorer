5e93735773345d5d2c6435360b4561f8413c7c2b
Brian Bartholomew <bartholomewbd@gmail.com>
2019-05-24 09:02:16 -0400

    Updates

---
+++ precache-manifest.d69bba3fd4f663243508e5afb4099674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d01f248e389525373a33e19b46f215",
+    "url": "/meteorite-explorer/index.html"
+  },
+  {
+    "revision": "b07d27310c6720186704",
+    "url": "/meteorite-explorer/static/css/main.235b14f6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/meteorite-explorer/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b07d27310c6720186704",
+    "url": "/meteorite-explorer/static/js/main.4d3cb58a.chunk.js"
+  },
+  {
+    "revision": "c602ff4126988d3b7f26",
+    "url": "/meteorite-explorer/static/js/runtime~main.c9db95ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b9948db04d8208f0ee3f9da6afcd9de.js"
+  "/meteorite-explorer/precache-manifest.d69bba3fd4f663243508e5afb4099674.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meteorite-explorer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
